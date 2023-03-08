990240b0eed2818e7c6e604079e8fafb460fc08a
Kerriea-star <ojared511@gmail.com>
2023-03-08 07:40:23 +0300

    ml registry

---
+++ apps/ml/registry.py
@@ -0,0 +1,34 @@
+from apps.endpoints.models import Endpoints
+from apps.endpoints.models import MLModel
+from apps.endpoints.models import MLModelStatus
+
+class MLRegistry:
+    def __init__(self):
+        self.endpoints = {}
+
+    def add_model(
+            self, endpoint_name, model_object, model_name,
+            model_status, model_version, owner, model_description,
+            model_code
+    ):
+        # get endpoint
+        endpoint, = Endpoints.objects.get_or_create(name=endpoint_name, owner=owner)
+
+        # get model
+        database_object, model_created = MLModel.objects.get_or_create(
+            name=model_name,
+            description = model_description,
+            code = model_code,
+            version = model_version,
+            owner = owner,
+            parent_endpoint=endpoint
+        )
+
+        if model_created:
+            status = MLModelStatus(status=model_status,
+                                   created_by=owner,
+                                   parent_mlmodel=database_object,
+                                   active=True)
+            status.save()
+        # add to registry
+        self.endpoints[database_object.id] = model_object
