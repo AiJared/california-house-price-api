a13b119aeece503cfc1b7b53e9ca2334f19bd82a
Kerriea-star <ojared511@gmail.com>
2023-03-08 12:25:29 +0300

    fixes

--- apps/endpoints/views.py
+++ apps/endpoints/views.py
@@ -78,7 +78,7 @@ class PredictView(views.APIView):
             )
         
         model_index = 0
-        algorithm_object = registry.endpoints[models[model_index].id]
+        algorithm_object = models[model_index].model
         model_object = algorithm_object['model']
         prediction = model_object.predict(request.data)
 
