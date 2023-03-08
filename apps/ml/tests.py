40e158239fb8c87be1b2abe6fb044448764ec33b
Kerriea-star <ojared511@gmail.com>
2023-03-08 07:56:25 +0300

    test registry

--- apps/ml/tests.py
+++ apps/ml/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+import inspect
 
+from apps.ml.registry import MLRegistry
 from apps.ml.price_prediction.random_forest import RandomForestRegressor
 
 class MLTests(TestCase):
@@ -19,4 +21,21 @@ class MLTests(TestCase):
         response = my_model.compute_prediction(input_data)
         self.assertEqual("OK", response["status"])
         self.assertTrue("predicted_value" in response)
-        self.assertAlmostEqual(358500, response["predicted_value"], places=5)
+        self.assertAlmostEqual(358500, response["predicted_value"], places=3)
+
+    def test_registry(self):
+        registry = MLRegistry()
+        self.assertEqual(len(registry.endpoints), 0)
+        endpoint_name = "price_prediction"
+        model_object = RandomForestRegressor()
+        model_name = "random forest"
+        model_status = "production"
+        model_version = "0.0.1"
+        model_owner = "Jared"
+        model_description = "Random Forest with simple pre- and post processing"
+        model_code = inspect.getsource(RandomForestRegressor)
+        # add to registry
+        registry.add_model(endpoint_name, model_object, model_name, model_status,
+                           model_version, model_owner, model_description, model_code)
+        # there should be one endpoint available
+        self.assertEqual(len(registry.endpoints), 1)
