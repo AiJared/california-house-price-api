5e9bcb57940ccb4c3833d90c3fabc7609290cc60
Kerriea-star <ojared511@gmail.com>
2023-03-08 07:19:38 +0300

    tests are working fine

--- apps/ml/tests.py
+++ apps/ml/tests.py
@@ -5,18 +5,18 @@ from apps.ml.price_prediction.random_forest import RandomForestRegressor
 class MLTests(TestCase):
     def test_rf_model(self):
         input_data = {
-            "longitude": -122.23,
-            "latitude": 37.88,
-            "housing_median_age": 41,
-            "total_rooms": 880,
-            "total_bedrooms": 129.0,
-            "population": 322,
-            "households": 126,
-            "median_income": 8.3252,
+            "longitude": -122.22,
+            "latitude": 37.86,
+            "housing_median_age": 21,
+            "total_rooms": 7099,
+            "total_bedrooms": 1106.0,
+            "population": 2401,
+            "households": 1138,
+            "median_income": 8.3014,
             "ocean_proximity": 0
         }
         my_model = RandomForestRegressor()
         response = my_model.compute_prediction(input_data)
         self.assertEqual("OK", response["status"])
         self.assertTrue("predicted_value" in response)
-        self.assertAlmostEqual(452600, response["predicted_value"], places=3)
+        self.assertAlmostEqual(358500, response["predicted_value"], places=5)
